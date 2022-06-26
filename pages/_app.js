b1d7cbd9b1ad96b234f21328720ad84660cf757d
Alex Otoo <alexotoous@outlook.com>
2022-06-26 07:50:10 -0400

    ico to png

--- pages/_app.js
+++ pages/_app.js
@@ -5,24 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
+        <link rel="shortcut icon" href="/images/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>
