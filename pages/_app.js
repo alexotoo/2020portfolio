bfeaec522d6f5e3cae7c9357e655d1fccc0676df
Alex Otoo <alexotoous@outlook.com>
2022-10-08 22:00:21 -0400

    GA4 added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
 import "../styles/global.scss";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=NEXT_PUBLIC_GA4_ID"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'NEXT_PUBLIC_GA4_ID');
+      </script>
       <Head>
         <link rel="shortcut icon" href="/images/favicon.png" />
       </Head>
