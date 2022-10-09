f678110a0944c151dc40db72b50eaf4423e9d4ab
Alex Otoo <alexotoous@outlook.com>
2022-10-08 22:13:32 -0400

    next script component added

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,15 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <script
+      <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=NEXT_PUBLIC_GA4_ID"
-      ></script>
-      <script>
+      ></Script>
+      <Script>
         window.dataLayer = window.dataLayer || []; function gtag()
         {dataLayer.push(arguments)}
         gtag('js', new Date()); gtag('config', 'NEXT_PUBLIC_GA4_ID');
-      </script>
+      </Script>
       <Head>
         <link rel="shortcut icon" href="/images/favicon.png" />
       </Head>
